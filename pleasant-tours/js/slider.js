8006ecea629384711141b5c478e75c1927539f98
Ggero Mpyangu <nathanmpyangu@users.noreply.github.com>
2019-11-08 11:39:51 +0300

    Add pleasant tours to files

---
+++ pleasant-tours/js/slider.js
@@ -0,0 +1,9 @@
+var unslider = $('div.slider').unslider();
+
+/*$('.unslider-arrow').click(function() {
+    var fn = this.className.split(' ')[1];
+
+    //  Either do unslider.data('unslider').next() or .prev() depending on the className
+    unslider.data('unslider')[fn]();
+});
+*/
